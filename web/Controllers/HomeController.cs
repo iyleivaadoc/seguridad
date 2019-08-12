19c1a0d7662c902d4741e553454cf2943f879041
ileiva <ileiva@IYLEIVA>
2019-08-12 12:02:23 -0600

    Add project files.

---
+++ web/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.saludo = Resourses.Strings.saludo;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.saludo = Resourses.Strings.saludo;
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Change()
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");
+            ViewBag.saludo = Resourses.Strings.saludo;
+            return View();
+        }
+    }
+}

614a86cb8cf1169a78a3a46d8fd4c3dec3e00fe8
mmartir <mstanley22@gmail.coom>
2019-08-21 16:33:02 -0600

    subiendo archivos relacionados a accesos

---
+++ web/Models/Accesos.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace web.Models
+{
+    public class Accesos
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id_acceso { get; set; }
+
+        [Required]
+        [Display(Name = "Nombre")]
+        [StringLength(50)]
+        public String Nombre { get; set; }
+
+        [Required]
+        [Display(Name = "Control")]
+        [StringLength(50)]
+        public String Control { get; set; }
+
+        [Required]
+        [Display(Name = "Metodo")]
+        [StringLength(50)]
+        public String Metodo { get; set; }
+
+        [Required]
+        [Display(Name = "Tipo")]
+        public Boolean Tipo { get; set; }
+
+        //[Required]
+        [Display(Name = "AccesoPredecesor")]
+        [StringLength(50)]
+        public String AccesoPredecesor { get; set; }
+
+        //[Required]
+        public virtual ICollection<Permisos> Permisos { get; set; }
+
+        //public virtual ICollection<Accesos> AccesosList { get; set; }
+
+
+    }
+}
